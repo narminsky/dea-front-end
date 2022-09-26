0cb5134e6c7ea7e4ef394c4e1a15445248c7ff7e
Narmin Sky <82314949+narminsky@users.noreply.github.com>
2022-09-26 19:22:29 +0400

    clearing db

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
-$(document).ready(function() {
+$(document).ready(function () {
     if (localStorage.getItem("span") != null) {
         $("#user-data").text(localStorage.getItem("span"));
         $(".login").addClass("d-none");
         $(".signout").removeClass("d-none");
         $(".my-items").removeClass("d-none");
     } else {
+        localStorage.clear();
         $(".signout").addClass("d-none");
         $(".login").removeClass("d-none");
         $(".my-items").addClass("d-none");
     }
-    $(".signout").click(function() {
+    $(".signout").click(function () {
         localStorage.removeItem("span");
         window.location.assign("index.html");
     });
